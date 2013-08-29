7557f58c89470eb62b150050fc1c35782795654a
ingikim <bcmet365@daum.net>
2013-08-30 02:58:56 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialnote::Application.config.secret_token = 'b3049fbe97542c9e8ea7471cbf9d2292abca7df14af65888310e63a43d558fea71b34cb9eb577ad87737294bc38887c976f18945b8c07283b5f280967936b714'
